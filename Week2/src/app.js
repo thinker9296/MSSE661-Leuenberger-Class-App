6d6ce8116011dc714061c923a1297f780304c4ea
thinker9296 <thinker9296@gmail.com>
2020-11-01 12:12:11 -0700

    week 2 commit

---
+++ Week2/src/app.js
@@ -0,0 +1,14 @@
+console.log('Hello from the console!!');
+
+//Choose a random color
+const button = document.querySelector('button')
+const body = document.querySelector('body')
+const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
+
+body.style.backgroundColor = 'violet'
+button.addEventListener('click', changeBackground)
+
+function changeBackground(){
+const colorIndex= parseInt(Math.random()*colors.length)
+body.style.backgroundColor = colors[colorIndex]
+}
