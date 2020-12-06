cbeb6074183fc2b9011353a1c4dcef321ea08d05
thinker9296 <thinker9296@gmail.com>
2020-12-06 10:39:03 -0700

    update goals.service.js

--- Week6/public/src/goals.service.js
+++ Week6/public/src/goals.service.js
@@ -1,9 +1,9 @@
-const TASKS_API = `${BASE_API_URL}/goals`; // http://localhost:3000/api/goals
+const GOALS_API = `${BASE_API_URL}/goals`; // http://localhost:3000/api/goals
 
-const getGoals = () => _get(TASKS_API, OPTIONS_WITH_AUTH);
+const getGoals = () => _get(GOALS_API, OPTIONS_WITH_AUTH);
 
 const addGoal = (formData) =>
-  _post(TASKS_API, formData, DEFAULT_OPTIONS_WITH_AUTH);
+  _post(GOALS_API, formData, DEFAULT_OPTIONS_WITH_AUTH);
 
 const deleteGoal = (goalId) =>
-  _delete(`${TASKS_API}/${goalId}`, OPTIONS_WITH_AUTH);
+  _delete(`${GOALS_API}/${goalId}`, OPTIONS_WITH_AUTH);
